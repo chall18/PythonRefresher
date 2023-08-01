b1c3afcb0965cdcf919522e0ea8c72c2b494d0b8
Caitie Hall <cahall2@uh.edu>
2023-07-31 20:30:21 -0500

    Receiving user input in Python.

---
+++ receivingInput.py
@@ -0,0 +1,8 @@
+#initial test
+# name = input("What is your name?\n")
+# print("Hi " + name)
+
+#Ask two questions: What is the person's name and favorite color. Then print "[name] likes [color]
+name = input("What is your name?\n")
+favColor = input("What is your favorite color?\n")
+print(name + " likes " + favColor)
